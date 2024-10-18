840635ac0f8772e93b05d809d6a618876c6348b1
Ruchika <142651783+Ruchikaf@users.noreply.github.com>
2024-10-18 13:16:50 +0530

    Time: 0 ms (100%), Space: 41.7 MB (21.48%) - LeetHub

--- 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.java
+++ 1426-find-n-unique-integers-sum-up-to-zero/1426-find-n-unique-integers-sum-up-to-zero.java
@@ -1,18 +1,24 @@
 class Solution {
     public int[] sumZero(int n) {
-         int [] arr=new int[n];
-        //  int temp=n/2; //2
-        //    int start=0; 
-        //     int end=n-1;
-        int k=0;
-        for(int i=1;i<=n/2;i++){
-            arr[k++]= i;  
-            arr[k++]= -i;  
-           
+        int[] arr = new int[n];
+        int temp = n / 2;  // Starting with n/2 for the value assignment
+        int start = 0; 
+        int end = n - 1;
+        
+        // Fill from both ends towards the center
+        while (start < end) {
+            arr[start] = -temp;  // Negative value at the start
+            arr[end] = temp;     // Positive value at the end
+            temp--;              // Decrease temp for next iteration
+            start++;
+            end--;
         }
-        if(n%2 !=  0){
-            arr[k]=0;
+        
+        
+        if (n % 2 != 0) {
+            arr[start] = 0;
         }
-         return arr;
+        
+        return arr;
     }
 }
