417a9499088273b3b1130bca2a3760aba6647f4c
Ruchika <142651783+Ruchikaf@users.noreply.github.com>
2025-03-08 09:20:17 +0530

    Time: 1 ms (99.84%), Space: 61.5 MB (81.51%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit=0;
+        int curr=prices[0];
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<curr){
+                curr=prices[i];
+
+            }else{
+                maxprofit=(int) Math.max(maxprofit,prices[i]-curr);
+            }
+        }return maxprofit;
+    }
+}
